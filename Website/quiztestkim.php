ddf68c81e13b2304336ffd349610b263faf73c0d
KimberleyR1 <kim.roks2001@gmail.com>
2023-01-26 14:21:21 +0100

    cookies werken p quiztestkim.php

--- Website/quiztestkim.php
+++ Website/quiztestkim.php
@@ -2,7 +2,7 @@
 <?php
     $cookie_name = "sum";
     $cookie_value = 0;
-    // setcookie($cookie_name, $cookie_value, 0, /);
+    setcookie($cookie_name, $cookie_value);
 ?>
 
 <html lang="en">
@@ -17,11 +17,11 @@
     function add_to_sum($value)
     {
         $sum = $sum + $value;
-        // setcookie($cookie_name, $sum, 0, /);
+        setcookie($cookie_name, $sum);
     }
     function add_cookie($question, $val)
     {
-        add_cookie($q1, $_POST['$q1']);
+        set_cookie($question, $val);
     }
     ?>
 </head>
@@ -67,97 +67,97 @@
                 https://www.footprintcalculator.org/home/en
             -->
             <form action="quiztest.php" method="post" id="form">
-                <div class="Q1">
+            <div class="Q1">
                     <h3>How often do you eat animal-based products?</h3>
                     <ul class="options1">
-                        <li><input type="button" name="q1" value="1">Daily</label></li>
-                        <li><input type="button" name="q1" value="2">Often</li>
-                        <li><input type="button" name="q1" value="3">Sometimes</li>
-                        <li><input type="button" name="q1" value="4">Never</li>
+                        <li><input type="radio" name="q1" value="1">Daily</label></li>
+                        <li><input type="radio" name="q1" value="2">Often</li>
+                        <li><input type="radio" name="q1" value="3">Sometimes</li>
+                        <li><input type="radio" name="q1" value="4">Never</li>
                     </ul>
                 </div>
                 <div class="Q2">
                     <h3>How much money do you spend on takeaways and restaurants?</h3>
                     <ul class="options2">
-                        <li><input type="button" name="q2" value="1">€0</li>
-                        <li><input type="button" name="q2" value="2">€1 - €20</li>
-                        <li><input type="button" name="q2" value="3">€20 - €50</li>
-                        <li><input type="button" name="q2" value="4">€50 or higher</li>
+                        <li><input type="radio" name="q2" value="1">€0</li>
+                        <li><input type="radio" name="q2" value="2">€1 - €20</li>
+                        <li><input type="radio" name="q2" value="3">€20 - €50</li>
+                        <li><input type="radio" name="q2" value="4">€50 or higher</li>
                     </ul>
                 </div>
                 <div class="Q3">
                     <h3>What is your fuel usage per week?</h3>
                     <ul class="options3">
-                        <li><input type="button" name="q3" value="1" onclick="add_to_sum(10)">1 litre - 10 litres</li>
-                        <li><input type="button" name="q3" value="2" onclick="add_to_sum(10)">10 litres - 20 litres</li>
-                        <li><input type="button" name="q3" value="3" onclick="add_to_sum(10)">20 litres - 30 litres</li>
-                        <li><input type="button" name="q3" value="4" onclick="add_to_sum(10)">30 litres - 40 litres</li>
-                        <li><input type="button" name="q3" value="5" onclick="add_to_sum(10)">40 litres - 50 litres</li>
-                        <li><input type="button" name="q3" value="6" onclick="add_to_sum(10)">50 litres or higher</li>
-                        <li><input type="button" name="q3" value="7" >I don't have a car</li>
-                        <li><input type="button" name="q3" value="8">I only travel by public transport</li>
+                        <li><input type="radio" name="q3" value="1">1 litre - 10 litres</li>
+                        <li><input type="radio" name="q3" value="2">10 litres - 20 litres</li>
+                        <li><input type="radio" name="q3" value="3">20 litres - 30 litres</li>
+                        <li><input type="radio" name="q3" value="4">30 litres - 40 litres</li>
+                        <li><input type="radio" name="q3" value="5">40 litres - 50 litres</li>
+                        <li><input type="radio" name="q3" value="6">50 litres or higher</li>
+                        <li><input type="radio" name="q3" value="7">I don't have a car</li>
+                        <li><input type="radio" name="q3" value="8">I only travel by public transport</li>
                     </ul>
                 </div>
                 <div class="Q4">
                     <h3>How often do you go on holiday a year?</h3>
                     <ul class="options4">
-                        <li><input type="button" name="q4" value="1">0</li>
-                        <li><input type="button" name="q4" value="2">1 - 2</li>
-                        <li><input type="button" name="q4" value="3">3 - 4</li>
-                        <li><input type="button" name="q4" value="4">5 or higher</li>
+                        <li><input type="radio" name="q4" value="1">0</li>
+                        <li><input type="radio" name="q4" value="2">1 - 2</li>
+                        <li><input type="radio" name="q4" value="3">3 - 4</li>
+                        <li><input type="radio" name="q4" value="4">5 or higher</li>
                     </ul>
                 </div>
                 <div class="Q5">
                     <h3>What kind of house do you live in?</h3>
                     <ul class="options5">
-                        <li><input type="button" name="q5" value="1">Detached</li>
-                        <li><input type="button" name="q5" value="2">Semi-detached</li>
-                        <li><input type="button" name="q5" value="3">Flat</li>
-                        <li><input type="button" name="q5" value="4">Houseboat</li>
-                        <li><input type="button" name="q5" value="5">Villa</li>
+                        <li><input type="radio" name="q5" value="1">Detached</li>
+                        <li><input type="radio" name="q5" value="2">Semi-detached</li>
+                        <li><input type="radio" name="q5" value="3">Flat</li>
+                        <li><input type="radio" name="q5" value="4">Houseboat</li>
+                        <li><input type="radio" name="q5" value="5">Villa</li>
                     </ul>
                 </div>
                 <div class="Q6">
                     <h3>What is the energy label of your house?</h3>
                     <ul class="options6">
-                        <li><input type="button" name="q6" value="1">A+, A++, A+++, A++++</li>
-                        <li><input type="button" name="q6" value="2">A</li>
-                        <li><input type="button" name="q6" value="3">B, C</li>
-                        <li><input type="button" name="q6" value="4">D, E</li>
-                        <li><input type="button" name="q6" value="5">F, G</li>
-                        <li><input type="button" name="q6" value="6">I don't know</li>
+                        <li><input type="radio" name="q6" value="1">A+, A++, A+++, A++++</li>
+                        <li><input type="radio" name="q6" value="2">A</li>
+                        <li><input type="radio" name="q6" value="3">B, C</li>
+                        <li><input type="radio" name="q6" value="4">D, E</li>
+                        <li><input type="radio" name="q6" value="5">F, G</li>
+                        <li><input type="radio" name="q6" value="6">I don't know</li>
                     </ul>
                 </div>
                 <div class="Q7">
                     <h3>What is your gas usage per year?</h3>
                     <ul class="options7">
-                        <li><input type="button" name="q7" value="1" onclick="add_to_sum(900 * 1.78)">up to 900 m3</li>
-                        <li><input type="button" name="q7" value="2" onclick="add_to_sum(900 * 1.78)">900 m3 - 1500 m3</li>
-                        <li><input type="button" name="q7" value="3" onclick="add_to_sum(900 * 1.78)">1500 m3 - 2000 m3</li>
-                        <li><input type="button" name="q7" value="4" onclick="add_to_sum(900 * 1.78)">More than 2000 m3</li>
-                        <li><input type="button" name="q7" value="5">I don't know</li>
+                        <li><input type="radio" name="q7" value="1">up to 900 m3</li>
+                        <li><input type="radio" name="q7" value="2">900 m3 - 1500 m3</li>
+                        <li><input type="radio" name="q7" value="3">1500 m3 - 2000 m3</li>
+                        <li><input type="radio" name="q7" value="4">More than 2000 m3</li>
+                        <li><input type="radio" name="q7" value="5">I don't know</li>
                     </ul>
                 </div>
                 <div class="Q8">
                     <h3>What is your electricity usage per year?</h3>
                     <ul class="options8">
-                        <li><input type="button" name="q8" value="1" onclick="add_to_sum(2000 * 0,37)"> Up to 2000 kWh</li>
-                        <li><input type="button" name="q8" value="2" onclick="add_to_sum(2000 * 0,37)">2000 kWh - 2500 kWh</li>
-                        <li><input type="button" name="q8" value="3" onclick="add_to_sum(2000 * 0,37)">2500 kWh - 3000 kWh</li>
-                        <li><input type="button" name="q8" value="4" onclick="add_to_sum(2000 * 0,37)">3000 kWh - 3500 kWh</li>
-                        <li><input type="button" name="q8" value="5" onclick="add_to_sum(2000 * 0,37)">3500 kWh - 4000 kWh</li>
-                        <li><input type="button" name="q8" value="6" onclick="add_to_sum(2000 * 0,37)">4000 kWh or more</li>
-                        <li><input type="button" name="q8" value="7">I don't know</li>
+                        <li><input type="radio" name="q8" value="1">Up to 2000 kWh</li>
+                        <li><input type="radio" name="q8" value="2">2000 kWh - 2500 kWh</li>
+                        <li><input type="radio" name="q8" value="3">2500 kWh - 3000 kWh</li>
+                        <li><input type="radio" name="q8" value="4">3000 kWh - 3500 kWh</li>
+                        <li><input type="radio" name="q8" value="5">3500 kWh - 4000 kWh</li>
+                        <li><input type="radio" name="q8" value="6">4000 kWh or more</li>
+                        <li><input type="radio" name="q8" value="7">I don't know</li>
                     </ul>
                 </div>
                 <div class="Q9">
                     <h3>Do you seperate your trash?</h3>
                     <ul class="options9">
-                        <li><input type="button" name="q9" value="1">Almost everything</li>
-                        <li><input type="button" name="q9" value="2">Only organic waste</li>
-                        <li><input type="button" name="q9" value="3">Only paper and plastic</li>
-                        <li><input type="button" name="q9" value="4">Only glass</li>
-                        <li><input type="button" name="q9" value="5">I don't sort my trash</li>
+                        <li><input type="radio" name="q9" value="1">Almost everything</li>
+                        <li><input type="radio" name="q9" value="2">Only organic waste</li>
+                        <li><input type="radio" name="q9" value="3">Only paper and plastic</li>
+                        <li><input type="radio" name="q9" value="4">Only glass</li>
+                        <li><input type="radio" name="q9" value="5">I don't sort my trash</li>
                     </ul>
                 </div>
                 <div class="last">
@@ -174,12 +174,19 @@
         // cookies misschien nodig voor resultpage
         } else if(isset($_POST['submit'])) {
                 $q1 = $_POST['q1'];
+                add_cookie('q1', $_POST['q1']);
                 $q2 = $_POST['q2'];
+                add_cookie('q2', $_POST['q2']);
                 $q3 = $_POST['q3'];
+                add_cookie('q3', $_POST['q3']);
                 $q4 = $_POST['q4'];
+                add_cookie('q4', $_POST['q4']);
                 $q5 = $_POST['q5'];
+                add_cookie('q5', $_POST['q5']);
                 $q6 = $_POST['q6'];
+                add_cookie('q6', $_POST['q6']);
                 $q7 = $_POST['q7'];
+                add_cookie('q7', $_POST['q7']);
 
                 echo "q1:" . $q1 . "\n";
                 echo "q2:" . $q2 . "\n";
