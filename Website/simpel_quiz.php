ba9e19905ca151a0147ca54afda4ff5fbfe56232
KimberleyR1 <kim.roks2001@gmail.com>
2023-01-31 12:36:54 +0100

    updated makecookie

--- Website/simpel_quiz.php
+++ Website/simpel_quiz.php
@@ -41,10 +41,10 @@ $(document).ready(function() {
 <body>
     <div class="container">
         <!-- div en dan end div? -->
-//	<button onclick="location.href='https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwixp5q00vH8AhUMH-wKHQdZCP0QwqsBegQIBxAB&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DdQw4w9WgXcQ&usg=AOvVaw0aHtehaphMhOCAkCydRLZU';" type="button">
+	<!-- <button onclick="location.href='https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwixp5q00vH8AhUMH-wKHQdZCP0QwqsBegQIBxAB&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DdQw4w9WgXcQ&usg=AOvVaw0aHtehaphMhOCAkCydRLZU';" type="button">
         <div class="content">
         </div>
-//	</button>
+	</button> -->
 
         <?php
         if(!isset($_POST["submit"])) {
@@ -55,11 +55,12 @@ $(document).ready(function() {
 		function q_add(id, c) {
 			TOTAL_CO2 += c;
 
-			// 7 komt overeen met het aantal vragen. 
+			// 7 komt overeen met het aantal vragen.
 			if (id.charAt(1) == "9") {
 				calc_done();
+                last();
 			 	return;
-			}	
+			}
 			next(id.charAt(1));
 		}
 		function calc_done() {
@@ -84,17 +85,25 @@ $(document).ready(function() {
 		}
 
 		function next(vraag_nummer) {
-			// nummer is char	
-            var next_q = `options${current_question + 1}`;
-            var class_name = `options${current_question}`;
+			// nummer is char
+            var next_q = `options${vraag_nummer + 1}`;
+            var class_name = `options${vraag_nummer}`;
 
-            var current_q = document.querySelector(`.Q${current_question}`);
-            var next_q = document.querySelector(`.Q${current_question + 1}`);
+            var current_q = document.querySelector(`.Q${vraag_nummer}`);
+            var next_q = document.querySelector(`.Q${vraag_nummer + 1}`);
 
             next_q.style.display = "block";
             current_q.style.display = "none";
 		}
-		
+        function last()
+        {
+            var current_q = document.querySelector(`.Q9`);
+            var next_q = document.querySelector(`.last`);
+
+            next_q.style.display = "block";
+            current_q.style.display = "none";
+        }
+
 		function make_cookie(currentQuestion, value)
 		{
 			document.cookie = `q${currentQuestion}` + value + "; expires=Thu, 18 Dec 2023 12:00:00 UTC; path=/; sameSite=none; secure; ";
@@ -110,102 +119,102 @@ $(document).ready(function() {
                 <div class="Q1">
                     <h3>How often do you eat animal-based products?</h3>
                     <ul class="options1">
-			<li onclick="q_add('q1a1', 0); make_cookie(1, 1);">Daily</li>
-                        <li onclick="q_add('q1a2', 350); make_cookie(1, 1);">Often</li>
-                        <li onclick="q_add('q1a3', 1050); make_cookie(1, 1);">Sometime</li>
-			<li onclick="q_add('q1a4', 1750); make_cookie(1, 1);">Never</li>
+		            	<li onclick="q_add('q1a1', 0); make_cookie(1, 1);">Daily</li>
+                        <li onclick="q_add('q1a2', 350); make_cookie(1, 2);">Often</li>
+                        <li onclick="q_add('q1a3', 1050); make_cookie(1, 3);">Sometime</li>
+			            <li onclick="q_add('q1a4', 1750); make_cookie(1, 4);">Never</li>
                     </ul>
                 </div>
                 <div class="Q2">
                     <h3>How much money do you spend on takeaways and restaurants?</h3>
                     <ul class="options2">
-                        <li onclick="q_add('q2a1', 0); make_cookie(1, 1);">€0</li>
-                        <li onclick="q_add('q2a2', 213.75); make_cookie(1, 1);">€1 - €20</li>
-                        <li onclick="q_add('q2a3', 356.25); make_cookie(1, 1);">€20 - €50</li>
-			<li onclick="q_add('q2a4', 498.75); make_cookie(1, 1);">€50 or higher</li>
-			<a onclick="prev(2)"><img src="img/feet_buttons/prev.png" id="prev2"></img></a>
+                        <li onclick="q_add('q2a1', 0); make_cookie(2, 1);">€0</li>
+                        <li onclick="q_add('q2a2', 213.75); make_cookie(2, 2);">€1 - €20</li>
+                        <li onclick="q_add('q2a3', 356.25); make_cookie(2, 3);">€20 - €50</li>
+			            <li onclick="q_add('q2a4', 498.75); make_cookie(2, 4);">€50 or higher</li>
+			            <a onclick="prev(2)"><img src="img/feet_buttons/prev.png" id="prev2"></img></a>
                     </ul>
                 </div>
 		        <div class="Q3">
                     <h3>What is your fuel usage per week?</h3>
                     <ul class="options3">
-                        <li onclick="q_add('q3a1', 1882.1); make_cookie(1, 1);">1 litre - 10 litres</li>
-                        <li onclick="q_add('q3a2', 811.25); make_cookie(1, 1);">10 litres - 20 litres</li>
-                        <li onclick="q_add('q3a3', 1135.75); make_cookie(1, 1);">20 litres - 30 litres</li>
-                        <li onclick="q_add('q3a4', 1460.25); make_cookie(1, 1);">30 litres - 40 litres</li>
-                        <li onclick="q_add('q3a5', 1784.75); make_cookie(1, 1);">40 litres - 50 litres</li>
-                        <li onclick="q_add('q3a6', 2109.25); make_cookie(1, 1);">50 litres or higher</li>
-                        <li onclick="q_add('q3a7', 2433.75); make_cookie(1, 1);">I don't have a car</li>
-                        <li onclick="q_add('q3a8', 2758.25); make_cookie(1, 1);">I only travel by public transport</li>
-			<a onclick="prev(3)"><img src="img/feet_buttons/prev.png" id="prev2"></img></a>
+                        <li onclick="q_add('q3a1', 1882.1); make_cookie(3, 1);">1 litre - 10 litres</li>
+                        <li onclick="q_add('q3a2', 811.25); make_cookie(3, 2);">10 litres - 20 litres</li>
+                        <li onclick="q_add('q3a3', 1135.75); make_cookie(3, 3);">20 litres - 30 litres</li>
+                        <li onclick="q_add('q3a4', 1460.25); make_cookie(3, 4);">30 litres - 40 litres</li>
+                        <li onclick="q_add('q3a5', 1784.75); make_cookie(3, 5);">40 litres - 50 litres</li>
+                        <li onclick="q_add('q3a6', 2109.25); make_cookie(3, 6);">50 litres or higher</li>
+                        <li onclick="q_add('q3a7', 2433.75); make_cookie(3, 7);">I don't have a car</li>
+                        <li onclick="q_add('q3a8', 2758.25); make_cookie(3, 8);">I only travel by public transport</li>
+			            <a onclick="prev(3)"><img src="img/feet_buttons/prev.png" id="prev2"></img></a>
                     </ul>
                 </div>
                 <div class="Q4">
                     <h3>How often do you go on holiday a year?</h3>
                     <ul class="options4">
-                        <li onclick="q_add('q4a1', 1500); make_cookie(1, 1);">0</li>
-                        <li onclick="q_add('q4a2', 6000); make_cookie(1, 1);">1 - 2</li>
-                        <li onclick="q_add('q4a3', 12000); make_cookie(1, 1);">3 - 4</li>
-                        <li onclick="q_add('q4a4', 18000); make_cookie(1, 1);">5 or higher</li>
-			<a onclick="prev(4)"><img src="img/feet_buttons/prev.png" id="prev2"></img></a>
+                        <li onclick="q_add('q4a1', 1500); make_cookie(4, 1);">0</li>
+                        <li onclick="q_add('q4a2', 6000); make_cookie(4, 2);">1 - 2</li>
+                        <li onclick="q_add('q4a3', 12000); make_cookie(4, 3);">3 - 4</li>
+                        <li onclick="q_add('q4a4', 18000); make_cookie(4, 4);">5 or higher</li>
+			            <a onclick="prev(4)"><img src="img/feet_buttons/prev.png" id="prev2"></img></a>
                     </ul>
                 </div>
                 <div class="Q5">
                     <h3>What kind of house do you live in?</h3>
                     <ul class="options5">
-                        <li onclick="q_add('q5a1', 0); make_cookie(1, 1);">Detached</li>
-                        <li onclick="q_add('q5a2', 21.9); make_cookie(1, 1);">Semi-detached</li>
-                        <li onclick="q_add('q5a3', 43.8); make_cookie(1, 1);">Flat</li>
-                        <li onclick="q_add('q5a4', 65.7); make_cookie(1, 1);">Houseboat</li>
-                        <li onclick="q_add('q5a5', 87.6); make_cookie(1, 1);">Villa</li>
-			<a onclick="prev(5)"><img src="img/feet_buttons/prev.png" id="prev2"></img></a>
+                        <li onclick="q_add('q5a1', 0); make_cookie(5, 1);">Detached</li>
+                        <li onclick="q_add('q5a2', 21.9); make_cookie(5, 2);">Semi-detached</li>
+                        <li onclick="q_add('q5a3', 43.8); make_cookie(5, 3);">Flat</li>
+                        <li onclick="q_add('q5a4', 65.7); make_cookie(5, 4);">Houseboat</li>
+                        <li onclick="q_add('q5a5', 87.6); make_cookie(5, 5);">Villa</li>
+            			<a onclick="prev(5)"><img src="img/feet_buttons/prev.png" id="prev2"></img></a>
                     </ul>
                 </div>
                 <div class="Q6">
                     <h3>What is the energy label of your house?</h3>
                     <ul class="options6">
-                        <li onclick="q_add('q6a1', 0); make_cookie(1, 1);">A+, A++, A+++, A++++</li>
-                        <li onclick="q_add('q6a2', 0); make_cookie(1, 1);">A</li>
-                        <li onclick="q_add('q6a3', 0); make_cookie(1, 1);">B, C</li>
-                        <li onclick="q_add('q6a4', 0); make_cookie(1, 1);">D, E</li>
-                        <li onclick="q_add('q6a5', 0); make_cookie(1, 1);">F, G</li>
-                        <li onclick="q_add('q6a6', 0); make_cookie(1, 1);">I don't know</li>
-			<a onclick="prev(6)"><img src="img/feet_buttons/prev.png" id="prev2"></img></a>
+                        <li onclick="q_add('q6a1', 0); make_cookie(6, 1);">A+, A++, A+++, A++++</li>
+                        <li onclick="q_add('q6a2', 0); make_cookie(6, 2);">A</li>
+                        <li onclick="q_add('q6a3', 0); make_cookie(6, 3);">B, C</li>
+                        <li onclick="q_add('q6a4', 0); make_cookie(6, 4);">D, E</li>
+                        <li onclick="q_add('q6a5', 0); make_cookie(6, 5);">F, G</li>
+                        <li onclick="q_add('q6a6', 0); make_cookie(6, 6);">I don't know</li>
+			            <a onclick="prev(6)"><img src="img/feet_buttons/prev.png" id="prev2"></img></a>
                     </ul>
                 </div>
                 <div class="Q7">
                     <h3>What is your gas usage per year?</h3>
                     <ul class="options7">
-		        <li onclick="q_add('q7a1', 0); make_cookie(1, 1);">up to 900 m3</li>
-                        <li onclick="q_add('q7a2', 0); make_cookie(1, 1);">900 m3 - 1500 m3</li>
-                        <li onclick="q_add('q7a3', 0); make_cookie(1, 1);">1500 m3 - 2000 m3</li>
-                        <li onclick="q_add('q7a4', 0); make_cookie(1, 1);">More than 2000 m3</li>
-                        <li onclick="q_add('q7a5', 0); make_cookie(1, 1);">I don't know</li>
-			<a onclick="prev(7)"><img src="img/feet_buttons/prev.png" id="prev2"></img></a>
+		                <li onclick="q_add('q7a1', 0); make_cookie(7, 1);">up to 900 m3</li>
+                        <li onclick="q_add('q7a2', 0); make_cookie(7, 2);">900 m3 - 1500 m3</li>
+                        <li onclick="q_add('q7a3', 0); make_cookie(7, 3);">1500 m3 - 2000 m3</li>
+                        <li onclick="q_add('q7a4', 0); make_cookie(7, 4);">More than 2000 m3</li>
+                        <li onclick="q_add('q7a5', 0); make_cookie(7, 5);">I don't know</li>
+			            <a onclick="prev(7)"><img src="img/feet_buttons/prev.png" id="prev2"></img></a>
                     </ul>
                 </div>
                 <div class="Q8">
                     <h3>What is your electricity usage per year?</h3>
                     <ul class="options8">
-		        <li onclick="q_add('q8a1', 0); make_cookie(1, 1);">Up to 2000 kWh
-                        <li onclick="q_add('q8a2', 0); make_cookie(1, 1);">2000 kWh - 2500 kWh
-                        <li onclick="q_add('q8a3', 0); make_cookie(1, 1);">2500 kWh - 3000 kWh
-                        <li onclick="q_add('q8a4', 0); make_cookie(1, 1);">3000 kWh - 3500 kWh
-                        <li onclick="q_add('q8a5', 0); make_cookie(1, 1);">3500 kWh - 4000 kWh
-                        <li onclick="q_add('q8a6', 0); make_cookie(1, 1);">4000 kWh or more
-                        <li onclick="q_add('q8a7', 0); make_cookie(1, 1);">I don't know
-			<a onclick="prev(8)"><img src="img/feet_buttons/prev.png" id="prev2"></img></a>
+		                <li onclick="q_add('q8a1', 0); make_cookie(8, 1);">Up to 2000 kWh
+                        <li onclick="q_add('q8a2', 0); make_cookie(8, 2);">2000 kWh - 2500 kWh
+                        <li onclick="q_add('q8a3', 0); make_cookie(8, 3);">2500 kWh - 3000 kWh
+                        <li onclick="q_add('q8a4', 0); make_cookie(8, 4);">3000 kWh - 3500 kWh
+                        <li onclick="q_add('q8a5', 0); make_cookie(8, 5);">3500 kWh - 4000 kWh
+                        <li onclick="q_add('q8a6', 0); make_cookie(8, 6);">4000 kWh or more
+                        <li onclick="q_add('q8a7', 0); make_cookie(8, 7);">I don't know
+			            <a onclick="prev(8)"><img src="img/feet_buttons/prev.png" id="prev2"></img></a>
                     </ul>
                 </div>
                 <div class="Q9">
                     <h3>Do you seperate your trash?</h3>
                     <ul class="options9">
-			<li onclick="q_add('q9a1', 0); make_cookie(1, 1);">Almost everything
-                        <li onclick="q_add('q9a2', 0); make_cookie(1, 1);">Only organic waste
-                        <li onclick="q_add('q9a3', 0); make_cookie(1, 1);">Only paper and plastic
-                        <li onclick="q_add('q9a4', 0); make_cookie(1, 1);">Only glass
-                        <li onclick="q_add('q9a5', 0); make_cookie(1, 1);">I don't sort my trash
-			<a onclick="prev(9)"><img src="img/feet_buttons/prev.png" id="prev2"></img></a>
+			            <li onclick="q_add('q9a1', 0); make_cookie(9, 1);">Almost everything
+                        <li onclick="q_add('q9a2', 0); make_cookie(9, 2);">Only organic waste
+                        <li onclick="q_add('q9a3', 0); make_cookie(9, 3);">Only paper and plastic
+                        <li onclick="q_add('q9a4', 0); make_cookie(9, 4);">Only glass
+                        <li onclick="q_add('q9a5', 0); make_cookie(9, 5);">I don't sort my trash
+			            <a onclick="prev(9)"><img src="img/feet_buttons/prev.png" id="prev2"></img></a>
                     </ul>
                 </div>
 
